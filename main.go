465d2624479b0a02bfa9d203ccb42876be748bbe
Chris Siannas <xrsia@otenet.gr>
2022-01-26 18:57:56 +0200

    Formatting matters

--- main.go
+++ main.go
@@ -5,23 +5,23 @@ import (
 )
 
 func Union(a, b []int) []int {
-      m := make(map[int]bool)
+	m := make(map[int]bool)
 
-      for _, item := range a {
-              m[item] = true
-      }
+	for _, item := range a {
+		m[item] = true
+	}
 
-      for _, item := range b {
-              if _, ok := m[item]; !ok {
-                      a = append(a, item)
-              }
-      }
-      return a
+	for _, item := range b {
+		if _, ok := m[item]; !ok {
+			a = append(a, item)
+		}
+	}
+	return a
 }
 
 func main() {
 	// 2 slices of int to Union them
-      var a = []int{1, 2, 3, 4, 5}
-      var b = []int{2, 3, 5, 7, 11}
-      fmt.Println(Union(a, b))
+	var a = []int{1, 2, 3, 4, 5}
+	var b = []int{2, 3, 5, 7, 11}
+	fmt.Println(Union(a, b))
 }
